2918c39a988e28db9dfae05ce556ee135812a41e
Miguelrpo09 <72237259+miguelrpo09@users.noreply.github.com>
2023-09-13 16:19:55 -0500

    tarea ciclos

---
+++ sections/tarea.ts
@@ -0,0 +1,11 @@
+// imprimir la tabla del 10 hasta el 50 con un ciclo
+
+let base = 10 ;
+
+let limite = 50;
+
+for ( let i = 1; i <= limite; i++){
+    console.log( base, ' X ', i, ' = ', base * i )
+}
+
+console.log('-----------Fin del programa----------');
